b22146a316bf0433b61502597ffc3bcc4d2200ab
olenevka <olenevka@G5200-W-V00047>
2024-08-30 09:28:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.Pdf;
+using Syncfusion.XlsIO;
+using Syncfusion.DocIORenderer;
+using Syncfusion.XlsIORenderer;
+
+namespace DocumentConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertDocxToPdf("файл.docx", "файл.pdf");
+        }
+
+        static void ConvertDocxToPdf(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream inputStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read))
+            {
+                WordDocument document = new WordDocument(inputStream, FormatType.Docx);
+
+                DocIORenderer docRenderer = new DocIORenderer();
+                using (PdfDocument pdfDocument = docRenderer.ConvertToPDF(document))
+                {
+                    using (FileStream outputStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write))
+                    {
+                        pdfDocument.Save(outputStream);
+                    }
+                }
+            }
+        }
+                
+    }
+}
